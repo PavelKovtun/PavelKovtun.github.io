638e8a11a2d18d774e4a45de8b360c519b4d2264
TominaDasha <dariya.tomina@mail.ru>
2017-11-23 18:20:28 +0500

    gallery

--- js/lightbox.js
+++ js/lightbox.js
@@ -9,11 +9,19 @@ for (var i = 1; i < 6; i++)
 {
 	document.getElementById('photo' + i).onclick = requestToShow;
 }
-
+window.onload = checkCookie();
 var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
+	deleteCookie("lastImage");
 	somethingShowed = false;
+}
+
+function checkCookie(){
+var cookie = getCookie("lastImage");
+if (cookie != null){
+	document.getElementById("photo" + cookie).click();
+	}
 }
 
 addEventListener("keydown", function(event) {
@@ -35,7 +43,16 @@ addEventListener("keydown", function(event) {
 		requestNum -= 1;
 		document.getElementById("photo" + requestNum).click();
 	}
-  });
+	if (event.keyCode == 32){
+		var body = document.getElementById("body");
+		body.style.backgroundImage = "url({0})".replace("{0}", document.getElementById("full" + requestNum).src);
+		document.cookie = "background={0}".replace("{0}", document.getElementById("full" + requestNum).src);
+		}
+	if (event.keyCode == 107)
+	{
+		alert(document.cookie);
+	}
+});
  
 function createFullPhoto(num){
     var modal = document.getElementById("modal");
@@ -48,6 +65,25 @@ function createFullPhoto(num){
     modal.insertBefore(modalImg, modal.firstChild);
 }
 
+function deleteCookie(name) {
+    var cookieDate = new Date();
+    cookieDate.setTime(cookieDate.getTime() - 1);
+    var cookie = name += "=; expires=" + cookieDate.toGMTString();
+    document.cookie = cookie;
+}
+
+function setCookie(){
+    somethingShowed = true;
+    document.cookie = "lastImage=" + requestNum;
+}
+
+function getCookie(name) {
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
 function requestToShow(event){
 	target = event.target || event.srcElement;
 	requestNum = parseInt(target.id[5]);
@@ -56,6 +92,7 @@ function requestToShow(event){
 	{
     modalImg.src = "img/photo" + target.id[5] + ".jpeg";
 	somethingShowed = true;
+	setCookie();
 	return true;
 	}
 	else
@@ -65,6 +102,7 @@ function requestToShow(event){
 	modalImg.style.height = '318px';
 	}
 	createFullPhoto(target.id[5])
+	setCookie();
 }
 
 function showLoad(event)
