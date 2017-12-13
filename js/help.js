6313c8b8119f75d849a2dac51a90fea680af4419
PavelKovtun <33497414+PavelKovtun@users.noreply.github.com>
2017-12-13 18:59:43 +0500

    Update help.js

--- js/help.js
+++ js/help.js
@@ -1,21 +1,13 @@
 var startLocation = window.location.href;
 var helper;
-document.onhlep=new Function(“return false”);
-window.onhelp=new Function(“return false”);
+document.onhelp = function() {return(false);}
+window.onhelp = function() {return(false);}
 if (startLocation.substr(-1) == "#") { helper = "window-blackout";}
 else {
 	helper = "#window-blackout";
 }
 addEventListener("keydown", function(event) {
     if (event.keyCode == 112)
-		window.location.href = startLocation + helper;
+	window.location.href = startLocation + helper;
   });
-
-window.addEventListener("keydown",function(event) {
-if (event.keyCode === 112) { 
-	if ( window.event ) {
-		window.event.keyCode = 0;
-	}
-}
-})
  
